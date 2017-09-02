834e13286ae22727e72e8e5f280b342056a92753
rnburn <ryan.burn@gmail.com>
2017-09-01 19:21:23 -1000

    Add opentracing as dependency to customized Makefile rule for LightStep.

--- source/common/tracing/lightstep_tracer_impl.cc
+++ source/common/tracing/lightstep_tracer_impl.cc
@@ -16,21 +16,51 @@ namespace Envoy {
 namespace Tracing {
 
 LightStepDriver::LightStepTransporter::LightStepTransporter(LightStepDriver& driver)
-  : driver_(driver) {}
+    : driver_(driver) {}
 
 void LightStepDriver::LightStepTransporter::Send(
-    const google::protobuf::Message& /*request*/, google::protobuf::Message& response,
+    const google::protobuf::Message& request, google::protobuf::Message& response,
     void (*on_success)(void* context), void (*on_failure)(std::error_code error, void* context),
     void* context) {
   on_success_callback_ = on_success;
   on_failure_callback_ = on_failure;
   active_response_ = &response;
   active_context_ = context;
+
+  Http::MessagePtr message =
+      Grpc::Common::prepareHeaders(driver_.cluster()->name(), lightstep::CollectorServiceFullName(),
+                                   lightstep::CollectorMethodName());
+  message->body() = Grpc::Common::serializeBody(request);
+
+  uint64_t timeout =
+      driver_.runtime().snapshot().getInteger("tracing.lightstep.request_timeout", 5000U);
+  driver_.clusterManager()
+      .httpAsyncClientForCluster(driver_.cluster()->name())
+      .send(std::move(message), *this, std::chrono::milliseconds(timeout));
 }
 
-void LightStepDriver::LightStepTransporter::onSuccess(Http::MessagePtr&&) {}
+void LightStepDriver::LightStepTransporter::onSuccess(Http::MessagePtr&& response) {
+  try {
+    Grpc::Common::validateResponse(*response);
 
-void LightStepDriver::LightStepTransporter::onFailure(Http::AsyncClient::FailureReason) {}
+    Grpc::Common::chargeStat(*driver_.cluster(), lightstep::CollectorServiceFullName(),
+                             lightstep::CollectorMethodName(), true);
+    if (!active_response_->ParseFromString(response->bodyAsString())) {
+      throw EnvoyException("Failed to parse LightStep collector response");
+    }
+    on_success_callback_(active_context_);
+  } catch (const Grpc::Exception& ex) {
+    Grpc::Common::chargeStat(*driver_.cluster(), lightstep::CollectorServiceFullName(),
+                             lightstep::CollectorMethodName(), false);
+    on_failure_callback_(std::error_code(), active_context_);
+  }
+}
+
+void LightStepDriver::LightStepTransporter::onFailure(Http::AsyncClient::FailureReason) {
+  Grpc::Common::chargeStat(*driver_.cluster(), lightstep::CollectorServiceFullName(),
+                           lightstep::CollectorMethodName(), false);
+  on_failure_callback_(std::error_code(), active_context_);
+}
 
 LightStepDriver::TlsLightStepTracer::TlsLightStepTracer(
     std::shared_ptr<opentracing::Tracer>&& tracer, LightStepDriver& driver)
